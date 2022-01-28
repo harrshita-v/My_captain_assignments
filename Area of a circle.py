f14d965038248ef6df7682de7e8bacabc433a9c5
harrshita-v <98258973+harrshita-v@users.noreply.github.com>
2022-01-28 20:31:45 +0530

    Update Area of a circle.py

--- Area of a circle.py
+++ Area of a circle.py
@@ -1,9 +1,3 @@
-#Assignment 1
-#1. The name float is used to store data of variables of floating numbers or decimals.
-# It can store, both a decimal and a whole number value.
-
-#2 the command used to open an application on command prompt is "start"
-# start is followed by the name of the app. Example, 'start spotify' will open the spotify app
 
 #Area of a circle
 r= float(input("Enter the radius:"))
