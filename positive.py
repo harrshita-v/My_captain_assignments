d6138abddda2e4e2bf2d6eba1f5a8fe8775c9469
harrshita-v <98258973+harrshita-v@users.noreply.github.com>
2022-02-03 18:25:49 +0530

    Add files via upload

---
+++ positive.py
@@ -0,0 +1,8 @@
+#print all positive numbers in a given range
+list1 = [-1, 21, -88, -450, 4, 77, 40]
+i = 0
+while (i < len(list1)):
+    if list1[i] >= 0:
+        print(list1[i], end=" ")
+    i += 1
+
